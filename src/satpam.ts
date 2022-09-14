fec624c524e6197ac30aa9fb8c2ca8c9de27e814
Ario Widiatmoko <ariowidiatmoko@gmail.com>
2022-09-14 21:56:13 +0700

    add refresh token check in session

--- src/satpam.ts
+++ src/satpam.ts
@@ -1,10 +1,13 @@
 import { CookieSerializeOptions } from 'cookie';
 import { parseCookies, serializeCookie } from './utility';
 
-export type OnValidationReturn = string | undefined | null;
+export type OnValidationData = {
+  token?: string,
+  refresh?: string
+} | null | undefined;
 
 /** Hook for validating token */
-export type validationFunction = (token: string) => OnValidationReturn | Promise<OnValidationReturn>;
+export type validationFunction = (token: OnValidationData) => OnValidationData | Promise<OnValidationData>;
 
 export type SatpamSession = {
   /** current session status */
@@ -15,11 +18,20 @@ export type SatpamSession = {
 
   /** cookie serialized token */
   serialized: string;
+
+  /** raw refresh token */
+  refresh?: {
+    token: string;
+    serialized: string;
+  };
 };
 
 export type SatpamOptions = {
   /** cookie name */
   name?: string;
+
+  /** refresh token name */
+  refresh?: string;
 
   /**
    * cookie options for cookie npm lib
@@ -41,11 +53,16 @@ export class Satpam {
   /** found token */
   private _token: string;
 
+  /** refresh token */
+  private _refreshToken: string;
+
   /** identifier for satpam instance */
   private _prefix: string;
 
   /** cookie name */
   private _name: string;
+
+  private _refreshName: string;
 
   private _cookieOptions: CookieSerializeOptions;
 
@@ -59,8 +76,13 @@ export class Satpam {
   constructor(prefix: string, options: SatpamOptions = {}) {
     this._prefix = prefix;
     this._name = options.name ?? 'satpam';
+    this._refreshName = options.refresh ?? 'refresh';
     this._cookieOptions = options.cookieOptions ?? {};
     this._validationHook = options.onValidation ?? null;
+
+    // set default
+    this._token = ''
+    this._refreshToken = ''
   }
 
   /** cookie max age setter */
@@ -98,6 +120,11 @@ export class Satpam {
     return [this._prefix, this._name].join('.');
   }
 
+  /** full refresh cookie name getter */
+  public get refreshCookie(): string {
+    return [this._prefix, this._refreshName].join('.');
+  }
+
   /** cookie name getter */
   public get name(): string {
     return this._name;
@@ -112,13 +139,30 @@ export class Satpam {
       token: this._token,
       status: this._token ? true : false,
       serialized: this._serializeToken(),
+      refresh: this.refresh ?? undefined
     };
+  }
+
+  public get refresh(): { token: string, serialized: string } {
+    return {
+      token: this._refreshToken,
+      serialized: this._refreshToken ? serializeCookie(this._refreshName, this._refreshToken, this._cookieOptions) : ''
+    }
   }
 
   /** serializing current token */
   private _serializeToken(): string {
     if (!this._token) return '';
     return serializeCookie(this.cookieName, this._token, this._cookieOptions);
+  }
+
+  private async _runHook(hook: validationFunction, token: string, refresh: string = '') {
+    if (typeof hook === 'function') {
+      const result = await hook({ token, refresh })
+      return result
+    }
+
+    return { token, refresh }
   }
 
   /**
@@ -129,21 +173,37 @@ export class Satpam {
    * @return {*}  {Promise<SatpamSession>}
    * @memberof Satpam
    */
-  private async _processToken(token: string): Promise<SatpamSession> {
-    if (typeof this._validationHook === 'function') {
-      token = (await this._validationHook(token)) ?? '';
+  private async _processToken(token: string, refresh: string = ''): Promise<SatpamSession> {
+    const result = await this._runHook(this._validationHook, token, refresh)
+
+    // set refresh if exist
+    if (result.refresh) this._refreshToken = result.refresh;
+
+    // set token if exist
+    if (result.token) this._token = result.token;
+
+    return this.session
+  }
+
+  /**
+   * url parameter check utility
+   *
+   * @private
+   * @param {string} params
+   * @return {string} token
+   * @memberof Satpam
+   */
+  private _urlParamCheck(params: string, key: string): string {
+    const parsed = params
+      .split('&')
+      .map((item) => item.split('='))
+      .reduce((acc, val) => ((acc[val[0]] = val[1]), acc), {});
+
+    if (parsed[key]) {
+      return parsed[key];
     }
 
-    if (token) {
-      this._token = token;
-      return {
-        token,
-        status: true,
-        serialized: this._serializeToken(),
-      };
-    }
-
-    return { status: false, serialized: '', token };
+    return '';
   }
 
   /**
@@ -157,13 +217,11 @@ export class Satpam {
   public async onCookies(cookies: string, onValidation: validationFunction = null): Promise<SatpamSession> {
     /** parsing cookies */
     const parsed = parseCookies(cookies);
-    let token = parsed[this.cookieName];
+    let token = parsed[this.cookieName] ?? this._token;
+    let refresh = parsed[this.refreshCookie] ?? this._refreshToken;
 
-    if (typeof onValidation === 'function') {
-      token = (await onValidation(token)) ?? '';
-    }
-
-    return this._processToken(token);
+    const result = await this._runHook(onValidation, token, refresh)
+    return this._processToken(result.token, result.refresh);
   }
 
   /**
@@ -172,6 +230,7 @@ export class Satpam {
    * @param {string} name
    * @param {(object)} headers
    * @param {validationFunction} [onValidation=null]
+   * @param {boolean} asRefresh set as refresh token
    * @return {*}  {Promise<SatpamSession>}
    * @memberof Satpam
    */
@@ -179,13 +238,15 @@ export class Satpam {
     name: string,
     headers: object | Headers,
     onValidation: validationFunction = null,
+    asRefresh: boolean = false
   ): Promise<SatpamSession> {
     // if headers name is cookie.. lol
     if (name.includes('cookie')) {
       return await this.onCookies(headers[name]);
     }
 
-    let token: string = '';
+    let token: string = this._token;
+    let refresh: string = this._refreshToken;
 
     // check if headers instanceof Headers / fetch.Headers
     if (typeof headers['has'] === 'function') {
@@ -196,11 +257,13 @@ export class Satpam {
       token = headers[name];
     }
 
-    if (typeof onValidation === 'function') {
-      token = (await onValidation(token)) ?? '';
+    if (asRefresh) {
+      refresh = token
+      token = this._token
     }
 
-    return this._processToken(token);
+    const result = await this._runHook(onValidation, token, refresh)
+    return this._processToken(result.token, result.refresh);
   }
 
   /**
@@ -209,11 +272,13 @@ export class Satpam {
    * @param {string} name
    * @param {(string | URL)} url
    * @param {validationFunction} [onValidation=null]
+   * @param {boolean} asRefresh set as refresh token
    * @return {*}  {Promise<SatpamSession>}
    * @memberof Satpam
    */
-  public async onUrl(name: string, url: string | URL, onValidation: validationFunction = null): Promise<SatpamSession> {
-    let token: string = '';
+  public async onUrl(name: string, url: string | URL, onValidation: validationFunction = null, asRefresh: boolean = false): Promise<SatpamSession> {
+    let token: string = this._token;
+    let refresh: string = this._refreshToken;
 
     if (typeof url === 'string') {
       // check on url queries
@@ -243,31 +308,15 @@ export class Satpam {
       }
     }
 
-    if (typeof onValidation === 'function') {
-      token = (await onValidation(token)) ?? '';
+    if (asRefresh) {
+      refresh = token
+      token = this._token
     }
 
-    return this._processToken(token);
+    const result = await this._runHook(onValidation, token, refresh)
+    return this._processToken(result.token, result.refresh);
   }
 
-  /**
-   * url parameter check utility
-   *
-   * @private
-   * @param {string} params
-   * @return {string} token
-   * @memberof Satpam
-   */
-  private _urlParamCheck(params: string, key: string): string {
-    const parsed = params
-      .split('&')
-      .map((item) => item.split('='))
-      .reduce((acc, val) => ((acc[val[0]] = val[1]), acc), {});
-
-    if (parsed[key]) {
-      return parsed[key];
-    }
-
-    return '';
-  }
+  // TODO:
+  // test with refresh token
 }
